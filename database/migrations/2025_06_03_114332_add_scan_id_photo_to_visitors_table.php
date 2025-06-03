ba57fab56e75e6cf0cc13827df14769f8db4cb32
MUHAMMAD TALHA JAVED <80463395+talha195javed@users.noreply.github.com>
2025-06-03 17:04:38 +0400

    Id scanning

---
+++ database/migrations/2025_06_03_114332_add_scan_id_photo_to_visitors_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('visitors', function (Blueprint $table) {
+            $table->string('scan_id_photo')->nullable()->after('id'); // Adjust "after" to the appropriate column
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('visitors', function (Blueprint $table) {
+            $table->dropColumn('scan_id_photo');
+        });
+    }
+};
